ea725a69a8aa1d07ed6f9149ffc2efd70633efa3
mohamed bouraoui <bouraoui99261855@gmail.com>
2024-06-26 12:02:58 +0100

    fix: app display

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,10 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const data = [1, 2, 3, 4, 56, 6];
+ 
   return (
     <div className="App">
-      {data.map((el: number) => (
-        <input />
-      ))}
+     MOHAMED
     </div>
   );
 }
