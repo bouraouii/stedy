61bfaffc15fc8c49f213b616cc3b562ccc24a1c2
mohamed bouraoui <bouraoui99261855@gmail.com>
2024-07-24 11:10:21 +0100

    feat: add `build`

--- src/Constant/data.ts
+++ src/Constant/data.ts
@@ -4,3 +4,13 @@ export const data = { name: "test@gmail.com", password: "12345" };
 export const generateUUID = (): string => {
   return uuidv4();
 };
+
+// git checkout gh-pages
+// git rm -rf .
+// git commit -m "Clean gh-pages branch"
+// git push origin gh-pages
+// git checkout main
+
+// git config --global user.name "Your Name"
+// git config --global user.email "you@example.com"
+// npm install gh-pages --save-dev
